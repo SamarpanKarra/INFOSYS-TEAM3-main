f3d97316cef21d0ff09cc67fd81e20deb810a115
samarpanKarra <samarpankarra@gmail.com>
2022-07-16 00:21:46 +0530

    first commit

---
+++ INFOSYS-TEAM3-main/team3.js
@@ -0,0 +1,143 @@
+function yes()
+{
+
+                document.getElementById("blocks").style.display="block";
+                document.getElementById("yes").style.backgroundColor="violet";
+                 document.getElementById("no").style.backgroundColor="white";
+
+            }
+    function no()
+    {
+
+                    document.getElementById("blocks").style.display="none";
+                    document.getElementById("no").style.backgroundColor="violet";
+                    document.getElementById("yes").style.backgroundColor="white";
+                   
+
+                }
+
+
+                function validate(){
+
+                    document.getElementById("kk").style.display="block" ;
+                    document.getElementById("g").style.backgroundColor="violet";
+                    document.getElementById("h").style.backgroundColor="white";
+                   
+
+                }
+                function validate1(){
+
+                  document.getElementById("kkk").style.display="block";
+                  document.getElementById("r").style.backgroundColor="violet";
+                  document.getElementById("b").style.backgroundColor="white";
+
+                }
+                function validate2(){
+
+                   document.getElementById("kkkk").style.display="block";
+                   document.getElementById("c").style.backgroundColor="violet";
+                   document.getElementById("d").style.backgroundColor="white";
+
+               }
+               function validate3(){
+
+                   document.getElementById("kkkkk").style.display="block";
+                   document.getElementById("e").style.backgroundColor="violet";
+                    document.getElementById("f").style.backgroundColor="white";
+                }
+
+
+                function validate4(){
+
+                document.getElementById("kk").style.display="none";
+                document.getElementById("g").style.backgroundColor="white";
+                document.getElementById("h").style.backgroundColor="violet";
+
+
+               }
+                function validate5(){
+
+              document.getElementById("kkk").style.display="none";
+              document.getElementById("r").style.backgroundColor="white";
+              document.getElementById("b").style.backgroundColor="violet";
+             }
+             function validate6(){
+
+              document.getElementById("kkkk").style.display="none";
+               document.getElementById("c").style.backgroundColor="white";
+              document.getElementById("d").style.backgroundColor="violet";
+            }
+             function validate7(){
+
+            document.getElementById("kkkkk").style.display="none";
+             document.getElementById("e").style.backgroundColor="white";
+            document.getElementById("f").style.backgroundColor="violet";
+             }
+
+
+
+
+
+                function validate8(){
+
+                    document.getElementById("aa").style.display="block" ;
+                    document.getElementById("i").style.backgroundColor="violet";
+                    document.getElementById("j").style.backgroundColor="white";
+                   
+
+                }
+                function validate10(){
+
+                  document.getElementById("aaa").style.display="block";
+                  document.getElementById("q").style.backgroundColor="violet";
+                  document.getElementById("l").style.backgroundColor="white";
+
+                }
+                function validate12(){
+
+                   document.getElementById("aaaa").style.display="block";
+                   document.getElementById("m").style.backgroundColor="violet";
+                   document.getElementById("n").style.backgroundColor="white";
+
+               }
+               function validate14(){
+
+                   document.getElementById("aaaaa").style.display="block";
+                   document.getElementById("o").style.backgroundColor="violet";
+                    document.getElementById("p").style.backgroundColor="white";
+                }
+
+
+                function validate9(){
+
+                document.getElementById("aa").style.display="none";
+                document.getElementById("i").style.backgroundColor="white";
+                document.getElementById("j").style.backgroundColor="violet";
+
+
+               }
+                function validate11(){
+
+              document.getElementById("aaa").style.display="none";
+              document.getElementById("q").style.backgroundColor="white";
+              document.getElementById("l").style.backgroundColor="violet";
+             }
+             function validate13(){
+
+              document.getElementById("aaaa").style.display="none";
+               document.getElementById("m").style.backgroundColor="white";
+              document.getElementById("n").style.backgroundColor="violet";
+            }
+             function validate15(){
+
+            document.getElementById("aaaaa").style.display="none";
+             document.getElementById("o").style.backgroundColor="white";
+            document.getElementById("p").style.backgroundColor="violet";
+             }
+
+
+
+
+
+
+
